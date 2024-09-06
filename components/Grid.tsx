8933382ba571ab0b59a106f59c6ccd186f933370
Foresight-Tech <Foresightagencies@gmail.com>
2024-09-06 21:56:50 +0100

    full project files

---
+++ components/Grid.tsx
@@ -0,0 +1,33 @@
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import MagicButton  from "./MagicButton";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid className="w-full py-20">
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={i}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          >
+            {item.id === 5 && (
+              <div className="mt-4">
+                <MagicButton href="/blog">View my Blog</MagicButton>
+              </div>
+            )}
+          </BentoGridItem>
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
---
+++ data/index.ts
@@ -0,0 +1,242 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "I foster innovation through secure, scalable solutions and clear, effective communication",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My Tech Stack",
+    description: "These are my most used technologies",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "grid grid-cols-3 gap-2 mt-4",
+    titleClassName: "justify-center",
+    // img: "",
+    // spareImg: "",
+    // techStack: [
+    //   // "React",
+    //   // "Node.js",
+    //   // "TypeScript",
+    //   // "GraphQL",
+    //   // "MongoDB",
+    //   // "Docker"
+    // ],
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for secure development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "The Inside Scoop",
+    description: "I write fun articles about software security",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Want to know more about my professional experience?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Mangospace",
+    des: "We revolutionalized Advertising with the use of a computer vision model that analyses views on traditional media like billboards to give advitisers better insights",
+    bannerImage: "/path/to/banner-image.jpg",
+    logo: "/path/to/project-logo.png",
+    liveUrl: "https://mangospace.tech",
+    img: "/p1.jpeg",
+    iconLists: [],
+    link: "https://mangospace.tech",
+  },
+  {
+    id: 2,
+    title: "Supplya",
+    des: "Supplya streamlining connections between wholesalers and small food vendors, offering valuable insights and efficient supply chain solutions",
+    bannerImage: "/path/to/banner-image.jpg",
+    logo: "/path/to/project-logo.png",
+    liveUrl: "https://supplya.shop",
+    img: "/p3.jpg",
+    iconLists: [],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Francheye",
+    des: "Francheye connects the best creators with the best brands in five clicks.",
+    bannerImage: "/path/to/banner-image.jpg",
+    logo: "/path/to/project-logo.png",
+    liveUrl: "https://Francheye.com",
+    img: "/p2.jpg",
+    iconLists: [],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "iPAS Consulting",
+    des: "Since 2016, iPAS Consulting has led the way in business management, driven by a client-focused approach and a team of experts. Our mission is to set new industry standards and achieve success for our clients.",
+    bannerImage: "/path/to/banner-image.jpg",
+    logo: "/path/to/project-logo.png",
+    liveUrl: "https://ipasng.org",
+    img: "/p4.jpg",
+    iconLists: [],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I had the pleasure of working with Osama for four years, and I can wholeheartedly say he was one of the most dedicated and talented professionals I've encountered. His commitment to excellence, combined with his ability to adapt to changing circumstances, was invaluable to our team. Osama was more than just an employee; he was a trusted partner who consistently contributed to our success.",
+    name: "Andrew Odia",
+    title: "Director of Human Resource iPAS",
+    image: "/andrew.jpeg", // Add this line
+  },
+  {
+    quote:
+      "Osama's expertise in system design and security was truly exceptional. His ability to anticipate potential challenges and develop effective solutions was instrumental in the success of our projects. Osama's commitment to quality and his leadership skills, timely delivery and great taste in music made him a valuable asset to my team.",
+    name: "Michael Sebastian",
+    title: "CEO, Francheye",
+    image: "/michael.jpeg", // Add this line
+  },
+  {
+    quote:
+      "Definitely one of my favorite employees with great banter. His ability to quickly adapt to new technologies, especially in the fast-paced world of e-commerce and cloud(Azure), was truly impressive. Osama didn’t just learn the systems; he mastered them, and in a short time, he was innovating and finding ways to fully exploit their capabilities. His innovative approach and quick learning curve brought a new level of efficiency and effectiveness to our projects.",
+    name: "Joy Adeniran Supplya",
+    title: "COO, Supplya",
+    image: "/joy.png", // Add this line
+  },
+  {
+    quote:
+      "Working with Osama at Mangospace was an incredible experience. The AI technology we were building was relatively new, and there weren't clear guidelines for the specific applications we aimed to achieve. However, every time we encountered blockers, Osama's creativity and problem-solving skills shone through. He consistently designed innovative solutions to overcome challenges, leading us to develop a world-class, high-performance system. His ability to navigate uncharted territories and turn obstacles into opportunities was key to the success of our project",
+    name: "Emmanuel Umoru",
+    title: "CEO Mangospace AI",
+    image: "/emmanuel.jpg", // Add this line
+  },
+];
+
+export const companies = [
+  //   {
+  //     id: 1,
+  //     name: "Nodejs",
+  //     img: "/cloud.svg",
+  //     nameImg: "/cloudName.svg",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "appwrite",
+  //     img: "/app.svg",
+  //     nameImg: "/appName.svg",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "HOSTINGER",
+  //     img: "/host.svg",
+  //     nameImg: "/hostName.svg",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "stream",
+  //     img: "/s.svg",
+  //     nameImg: "/streamName.svg",
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "docker.",
+  //     img: "/dock.svg",
+  //     nameImg: "/dockerName.svg",
+  //   },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Engineering Manager - Francheye",
+    desc: "Lead a team of 9 engineers, architected and built major parts of our backend microservices, optimized db queries with indexing and added caching strategies",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Lead Backend Developer - Supplya",
+    desc: "Built a robust NestJs microservice, integrated payments and deployed using azure app service, configured various backend environments and integrated ci/cd using github actions.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Backend Developer & Solutions Architect - Mangospace",
+    desc: "Architected and built the user managements service using laravel, worked with the head of AI to build out and integrate the AI model into our systems, used mongodb time series and AWS SQS for asynchronus communication between services",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Web Developer -  iPAS Consulting",
+    desc: "Worked with wordpress, html, css and react to build functional and responsive websites, took part in requirement gathering and brain storming sessions geared at solving our clients business problems using technology.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    name: "GitHub",
+    img: "/git.svg",
+    link: "https://github.com/osama575",
+  },
+  {
+    id: 2,
+    name: "Instagram",
+    img: "/twit.svg",
+    link: "https://www.instagram.com/foresight_tech_agency_/",
+  },
+  {
+    id: 3,
+    name: "LinkedIn",
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/osama-okunbo-a52101222/",
+  },
+];
