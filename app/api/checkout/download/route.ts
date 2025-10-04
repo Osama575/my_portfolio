c923594e7625a80ac9db9ce66361328eb1e6c1fc
Osama575 <foresightagencies@gmail.com>
2025-10-05 00:38:37 +0100

    fix issue3

--- app/api/checkout/download/route.ts
+++ app/api/checkout/download/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import path from "path";
 import fs from "fs/promises";
 
-// Route to download the PDF file to the client
+// Route to download the PDF file
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 
